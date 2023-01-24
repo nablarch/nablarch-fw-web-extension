a9829ef93cb419872c53e7a81ff6d2bc74c4ff4f
opengl-8080 <opengl-8080@users.noreply.github.com>
2023-01-24 16:03:32 +0900

    feat: javax -> jakarta

--- src/test/java/nablarch/fw/web/upload/util/TestCities.java
+++ src/test/java/nablarch/fw/web/upload/util/TestCities.java
@@ -1,9 +1,9 @@
 package nablarch.fw.web.upload.util;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 /**
  * コード名称
